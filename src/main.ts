87f106e09286589d2879e1aa1585169d24f40118
Ali Mert Çakar <mail@alimert.net>
2021-11-02 15:07:47 +0300

    code

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,17 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  //OpenAPI
+  const config = new DocumentBuilder()
+    .setTitle('Portfolio API')
+    .setDescription('The portfolio API description')
+    .setVersion('1.0')
+    .addTag('portfolio')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
